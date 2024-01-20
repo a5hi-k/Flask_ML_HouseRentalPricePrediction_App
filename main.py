8f87a2b4fe430c7a61555ecf006bb0664e38a108
Ashik <ashikca65@gmail.com>
2024-01-21 01:12:52 +0530

    second commit

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from application.controllers import app
 
 
 
+# hiding for production version
 
+# if __name__ == '__main__':
 
-if __name__ == '__main__':
-
-    app.run(debug=True)    
+#     app.run(debug=True)    
